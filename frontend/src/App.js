0522c4470d71cdc0b62546fbd983d17eef49bc5d
svsannidhay <sannidhayvashalsv@gmail.com>
2021-05-06 19:55:21 +0530

    responsive navigation added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import Home from './Home';
 import Supply from './Supply';
 import Demand from './Demand';
 import { Route, Link } from 'react-router-dom';
+import NavBar from './NavBar';
 
 class App extends React.Component{
   render() {
     return (
       <div>
+        <NavBar></NavBar>
         <Route exact path = "/" component = {Home} />
         <Route exact path = "/supply" component = {Supply} />
         <Route exact path = "/demand" component = {Demand} />
--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -3,7 +3,22 @@ import React from 'react';
 class NavBar extends React.Component {
   render() {
     return(
-      <div>Nav</div>
+      <div className = "navigation">
+        <input type="checkbox" className = "navigation__checkbox" id="navi-toggle"/>
+        <label for = "navi-toggle" className = "navigation__button">
+          <span class="navigation__icon">&nbsp;</span>
+        </label>
+        <div class="navigation__background">&nbsp;</div>
+        <nav class="navigation__nav">
+          <ul class="navigation__list">
+            <li class="navigation__item"><a href="/" class="navigation__link"><span>01</span>Home</a></li>
+            <li class="navigation__item"><a href="/supply" class="navigation__link"><span>02</span>Supply</a></li>
+            <li class="navigation__item"><a href="/demand" class="navigation__link"><span>03</span>Demand</a></li>                    
+            <li class="navigation__item"><a href="#" class="navigation__link"><span>04</span>Stories</a></li>
+            <li class="navigation__item"><a href="#" class="navigation__link"> <span>05</span> Book Now</a></li>                    
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
